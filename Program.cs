c2c555243a1f9b2f1576825e6edfa20fca2eec97
Renato Ferreira Soares <renato.soares@INTRANET.CORP>
2020-06-17 15:41:15 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int num=5;
+            for (int i=0;i<num;i++){
+                Console.WriteLine($"Loop {i}");
+            }
+        }
+    }
+}
